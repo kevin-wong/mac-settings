72d9027a699dd61940c0893a04b9fe159212311c
Kevin Wong <kevin@Kevin-TN.local>
2016-06-05 04:39:36 -0400

    commit

--- iTermConfigure.sh
+++ iTermConfigure.sh
@@ -3,3 +3,7 @@
 mv iTermSettings.plist com.googlecode.iterm2.plist
 cp com.googlecode.iterm2.plist ~/Library/Preferences
 defaults read com.googlecode.iterm2 > /dev/null
+
+mv ~/.bash_profile ~/.bash_profile.bak 2>/dev/null
+cp .bash_profile ~/
+
